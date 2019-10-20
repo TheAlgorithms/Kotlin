bcbd92065a6ed1d27e0055afae7fec1d21e8d2c7
MatanDavidi <davidi.matan@gmail.com>
2019-10-20 02:27:41 +0200

    Change return statements from if-else chain to when statement

--- src/main/kotlin/mathematics/Factorial.kt
+++ src/main/kotlin/mathematics/Factorial.kt
@@ -10,11 +10,9 @@ import java.security.InvalidParameterException
 fun getFactorial(number: Long): Long {
     if (number < 0L) {
         throw InvalidParameterException("The number of which to calculate the factorial must be greater or equal to zero.")
-    } else return if (number == 0) {
-        1
-    } else if (number == 1) {
-        number
-    } else {
-        number * getFactorial(number - 1)
+    } else return when (number) {
+        0L -> 1
+        1L -> number
+        else -> number * getFactorial(number - 1)
     }
 }
