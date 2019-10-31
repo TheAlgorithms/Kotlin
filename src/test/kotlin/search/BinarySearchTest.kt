a71edd43a32e85a0f13e1f42646e4fbcd356e29d
Emil Suleymanov <suleymanovemil8@gmail.com>
2019-10-31 23:54:12 +0100

    Reformat binary search test code (#16)

--- src/test/kotlin/search/BinarySearchTest.kt
+++ src/test/kotlin/search/BinarySearchTest.kt
@@ -6,28 +6,28 @@ class BinarySearchTest {
 
     @Test
     fun testBinarySearch1() {
-        val array = arrayOf(1, 2, 4, 8 , 16)
+        val array = arrayOf(1, 2, 4, 8, 16)
         val key = 2
         assert(binarySearch(array, key) == 1)
     }
 
     @Test
     fun testBinarySearch2() {
-        val array = arrayOf(1, 2, 4, 8 , 16)
+        val array = arrayOf(1, 2, 4, 8, 16)
         val key = 1
         assert(binarySearch(array, key) == 0)
     }
 
     @Test
     fun testBinarySearch3() {
-        val array = arrayOf(1, 2, 4, 8 , 16)
+        val array = arrayOf(1, 2, 4, 8, 16)
         val key = 16
         assert(binarySearch(array, key) == 4)
     }
 
     @Test
     fun testBinarySearch4() {
-        val array = arrayOf(1, 2, 4, 8 , 16)
+        val array = arrayOf(1, 2, 4, 8, 16)
         val key = 20
         assert(binarySearch(array, key) == -1)
     }
