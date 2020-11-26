254973503ad865c8c1c7d49504922f993fdbc6d2
laurelr2020 <reganlaurell@gmail.com>
2020-11-26 14:37:26 -0500

    create tests for average

--- src/test/kotlin/math/AverageTest.kt
+++ src/test/kotlin/math/AverageTest.kt
@@ -1,7 +1,29 @@
 package math
 
+import org.junit.Test
+
 class AverageTest {
-    fun testAverageDoubleArray() {
-//        val DoubleArray.arrayOf(1.0, 1.1, 1.2, )
+    @Test
+    fun testAverageArrayOfDouble() {
+        val doubleArray = arrayOf(1.0, 1.25, 1.50, 1.75, 2.0)
+        assert(average(doubleArray) == 1.50)
+    }
+
+    @Test
+    fun testAverageArrayOfDoubleWithOneElement() {
+        val doubleArray = arrayOf(1.0)
+        assert(average(doubleArray) == 1.00)
+    }
+
+    @Test
+    fun testAverageArrayOfInt() {
+        val intArray = arrayOf(1, 2, 3)
+        assert(average(intArray) == 2)
+    }
+
+    @Test
+    fun testAverageArrayOfIntWithOneElement() {
+        val intArray = arrayOf(1)
+        assert(average(intArray) == 1)
     }
 }
