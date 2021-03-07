6c92d414f3305934191f49286994608704ffa0d2
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 14:35:35 -0800

    add complexity analysis

--- src/main/kotlin/search/TernarySearch.kt
+++ src/main/kotlin/search/TernarySearch.kt
@@ -1,5 +1,13 @@
 package search
 
+/**
+ * Complexity Analysis:
+ * 
+ * Time Complexity: O(log3 n)
+ * Space Complexity: O(1)
+ * 
+ */
+
 fun ternarySearch(l: Double, r: Double, func: (Double) -> Double, eps: Double = 1e-3): Double {
     var left = l
     var right = r
