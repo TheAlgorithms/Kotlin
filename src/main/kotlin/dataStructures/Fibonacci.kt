1085de4b10b04284bb4cecbf83fb0146dc249c57
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 14:20:17 -0800

    Update Fibonacci.kt

--- src/main/kotlin/dataStructures/Fibonacci.kt
+++ src/main/kotlin/dataStructures/Fibonacci.kt
@@ -5,6 +5,17 @@ package dataStructures
  * such that each number is the sum of the two preceding ones, starting from 0 and 1
  */
 
+/**
+ * Complexity Analysis:
+ * 
+ * Time complexity: O(n). Each number, starting at 2 up to and
+ * including N, is visited, computed and then stored for O(1) access
+ * later on.
+ *
+ * Space complexity: O(n). The size of the stack in memory is
+ * proportionate to N.
+ */
+
 // Create mutableMap to save counted value for a specific index
 val answersMap: MutableMap<Long, Long> = mutableMapOf(0L to 0L, 1L to 1L)
 
