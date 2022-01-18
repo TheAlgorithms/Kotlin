bc3aa8dbd8479ea1592a99cfef24cf60e7f71bb8
Aman <aexyno@gmail.com>
2022-01-18 22:51:24 +0530

    Only documentation changes. Made all 'search' code doc in sync, formatted and more readable.

--- src/main/kotlin/search/BinarySearch.kt
+++ src/main/kotlin/search/BinarySearch.kt
@@ -3,9 +3,9 @@ package search
 /**
  * Binary search is an algorithm which finds the position of a target value within an array (Sorted)
  *
- * Worst-case performance	O(log(n))
- * Best-case performance	O(1)
- * Average performance	O(log(n))
+ * Worst-case performance	    O(log(n))
+ * Best-case performance	    O(1)
+ * Average performance	        O(log(n))
  * Worst-case space complexity	O(1)
  */
 
